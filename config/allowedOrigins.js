5bed286916d3ce0180f842ad206bf4a00e89fa4d
PommesRS <rspommes@gmail.com>
2024-01-27 23:00:56 +0100

    first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+const allowedOrigins = [
+    'https://www.DJRamsad.de',
+    'http://127.0.0.1:5500',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'http://192.168.2.109:5000'
+];
+
+module.exports = allowedOrigins;
