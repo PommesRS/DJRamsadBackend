bf056a7ab880f8adaed2169ef54b4bd98020f83c
PommesRS <rspommes@gmail.com>
2024-01-28 00:51:52 +0100

    added flexibility

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,8 +4,10 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
-    'http://192.168.2.109:5000'
-    //pimmel
+    'http://192.168.2.109:5000',
+    'https://djramsad.netlify.app',
+    `'${process.env.CUSTOM_CORS_URL1}'`,
+    `'${process.env.CUSTOM_CORS_URL2}'`
 ];
 
 module.exports = allowedOrigins;
